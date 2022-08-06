7763be25186168213629d6a8a857b3461daa7b63
jkkwon9 <jkkwon9@gmail.com>
2022-08-06 11:48:56 +0900

    변경1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, {component} from 'react';
 import './App.css';
 import Customer from './components/Customer'
 
 const customer  = {
     'id'   : '1',
-    'name' : '홍동',
+    'name' : '홍길동',
     'birthday' : '12322',
     'gendor' : '남자',
     'job' : '회사원',
@@ -11,13 +12,16 @@ const customer  = {
 
 function App() {
   return (
-    <Customer
-      id={customer.id}
-      name={customer.name}
-      birthday={customer.birthday}
-      gendor={customer.gendor}
-      job={customer.job}
-      />
+
+    <div>
+        <Customer
+           id={customer.id}
+           name={customer.name}
+           birthday={customer.birthday}
+          gendor={customer.gendor}
+          job={customer.job}
+        />
+   </div>
   );
 }
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,4 +1,6 @@
+
 import React from 'react';
+
 class Customer extends React.Component{
     render(){
         return (
@@ -9,20 +11,24 @@ class Customer extends React.Component{
         )        
     }
 }
-class CustomerProfile extends  React.Component{
-    rendor(){
-        <div>
-            <h2> {this.props.name}({this.props.id})  </h2>
-        </div>
+class CustomerProfile extends React.Component{
+    rendor(){ 
+        return (
+            <div>
+                <h2> {this.props.name}({this.props.id}) </h2>
+            </div>
+        )
     }
 }
-class Customerinfo extends  React.Component{
+class Customerinfo extends React.Component{
     rendor(){
-        <div>
-            <p> {this.props.birthday} </p>
-            <p> {this.props.gendor} </p>
-            <p> {this.props.job} </p>
-        </div>
+        return (
+            <div>
+                <p> {this.props.birthday} </p>
+                <p> {this.props.gendor} </p>
+                <p> {this.props.job} </p>
+            </div>
+        )
     }
 }
 export default Customer;
